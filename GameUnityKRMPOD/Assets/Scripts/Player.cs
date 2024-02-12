f53fac308a790c155a10ed5412ad957aa8baa7cf
Kiber Kotletka <115551713+Kiber-Kotletka@users.noreply.github.com>
2024-02-12 23:11:55 +0200

    Initial commit

---
+++ GameUnityKRMPOD/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public Transform groundCheck;
+    bool isGrounded;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+ 
+        CheckGround();
+        if (Input.GetAxis("Horizontal") == 0 && ( isGrounded)){
+            anim.SetInteger("State", 1);
+        }
+        else{
+            Flip();
+            if (isGrounded)
+                anim.SetInteger("State", 2);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        if (Input.GetAxis("Horizontal") < 0)
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+    }
+
+    void CheckGround()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f);
+        isGrounded = colliders.Length > 1;
+    }
+}
